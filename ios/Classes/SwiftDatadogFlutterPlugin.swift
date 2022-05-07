ebc54f2fcceca81ebcc57e69a6bcd9beca174fbb
Yuichi Ikeda <yuichi.ikeda@gmail.com>
2022-05-07 12:58:48 +0900

    use US3 site if useEUEndpoints enabled

--- ios/Classes/SwiftDatadogFlutterPlugin.swift
+++ ios/Classes/SwiftDatadogFlutterPlugin.swift
@@ -313,7 +313,7 @@ public class SwiftDatadogFlutterPlugin: NSObject, FlutterPlugin {
 
     config = config.set(serviceName: serviceName)
     if useEUEndpoints {
-      config = config.set(endpoint: .eu)
+      config = config.set(endpoint: .us3)
     }
 
     return config
